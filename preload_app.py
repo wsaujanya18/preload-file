6455b8097781bfd90fce75f20902acbe3ee2aa81
wsaujanya18 <46403434+wsaujanya18@users.noreply.github.com>
2023-08-18 10:51:15 +0545

    Update preload_app.py

--- preload_app.py
+++ preload_app.py
@@ -1,3 +1,36 @@
 import streamlit as st
+import pandas as pd
+import base64
 
-st.write("Preload")
+st.header("Preload JSON")
+st.write("Converts preload excel file to preload json file.")
+st.write("")
+
+def main():
+  st.title("CSV and Excel file Uploader")
+
+  # File uploader
+  uploaded_file = st.file_uploader("Uplaod a CSV or Excel file", type=["Csv", "xlsx"])
+
+  if uploaded_file is not None:
+    file_extension = uploaded_file.name.split(".")[-1]
+
+    if file_extension == "csv":
+      df = pd.read_csv(uploaded_file)
+    elif file_extension == "xlsx":
+      df = pd.read_excel(uploaded_file, engine="openpyxl")
+
+    # Show Dataframe
+    st.dataframe(df)
+
+    # Convert to JSON Button
+    if st.button("Convert to JSON"):
+      json_data = df.to_json(orient='records', force_ascii=False)
+      st.text_area("JSON Data", json_data, height=500)
+
+      # Download button for JSON data
+      st.download_button("Download JSON File", data=json_data, file_name=uploaded_file.name)
+
+
+if __name__ == "__main__":
+  main()
